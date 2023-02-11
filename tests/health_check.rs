8cf11fd5e4053f54f5662510cc98a70b4361df48
Samanyu <samanyu6@yahoo.co.in>
2023-02-11 19:30:25 +0530

    added subscriptions as a functionality

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,4 @@
 use std::net::TcpListener;
-
 use zero2prod::run;
 
 // tokio handles clean up of spawn under tokio::test and spins up new ones for each run
@@ -17,6 +16,57 @@ async fn health_check_works() {
 
     assert!(resp.status().is_success());
     assert_eq!(Some(0), resp.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_ok_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=sam%20b&email=sam_7%40gmail.com";
+    let resp = client
+        .post(&format!("{}/subscriptions", &app_address.await))
+        .header("Content-type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to exec request");
+
+    assert_eq!(
+        200,
+        resp.status().as_u16(),
+        "API failed with correct input {}",
+        body
+    );
+}
+
+#[tokio::test]
+async fn subscribe_returns_error_for_missing_form_data() {
+    let add = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let tests = vec![
+        ("name=sam%20b", "missing email"),
+        ("email=sam_7%40gmail.com", "missing name"),
+        ("", "missing both email and name"),
+    ];
+
+    for (body, msg) in tests {
+        let resp = client
+            .post(&format!("{}/subscriptions", &add))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            resp.status().as_u16(),
+            "API did not fail with 400 when a bad request {} was sent",
+            msg
+        );
+    }
 }
 
 async fn spawn_app() -> String {
